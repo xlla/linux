469bb99521544c5d37f16297a16ff5afbba4789a
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2016-12-22 04:44:32 +0200

    ASoC: Intel: hifi2: Get rid of snail address in comments
    Snail mail addresses are subject to change. This already happened once,
    so, better to get rid of mention of such addresses for good.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- sound/soc/intel/boards/mfld_machine.c
+++ sound/soc/intel/boards/mfld_machine.c
@@ -1,5 +1,5 @@
 /*
- *  mfld_machine.c - ASoc Machine driver for Intel Medfield MID platform
+ *  ASoC Machine driver for Intel Medfield MID platform
  *
  *  Copyright (C) 2010 Intel Corp
  *  Author: Vinod Koul <vinod.koul@intel.com>
@@ -14,10 +14,6 @@
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  *
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
