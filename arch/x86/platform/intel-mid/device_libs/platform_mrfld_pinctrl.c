0bf9f5491ea3df3425f686e4cbc10ae1c10ea658
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2017-08-28 16:36:35 +0300

    x86/platform/intel-mid: Don't enumerate devices when SFI is disabled
    There is no need to register drivers for the Intel MID platform devices
    when SFI is disabled at run-time by some reason.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_mrfld_pinctrl.c
+++ arch/x86/platform/intel-mid/device_libs/platform_mrfld_pinctrl.c
@@ -31,6 +31,9 @@ static struct platform_device mrfld_pinctrl_device = {
 
 static int __init mrfld_pinctrl_init(void)
 {
+	if (sfi_disabled)
+		return -EINVAL;
+
 	if (intel_mid_identify_cpu() == INTEL_MID_CPU_CHIP_TANGIER)
 		return platform_device_register(&mrfld_pinctrl_device);
 
