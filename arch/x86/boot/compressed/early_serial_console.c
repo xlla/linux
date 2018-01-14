9c19237949ac7b8bba8beaa63a4ec7fe696c0e7b
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2018-01-14 15:36:43 +0200

    x86/boot: Introduce helpers for serial I/O
    As preparatory to enable earlyprintk on non-standard ports on x86,
    introduce serial_in() and serial_out() helpers to perform serial I/O.
    
    No functional change intended.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- arch/x86/boot/compressed/early_serial_console.c
+++ arch/x86/boot/compressed/early_serial_console.c
@@ -1,5 +1,8 @@
 #include "misc.h"
 
+unsigned int (*serial_in)(unsigned long addr, int offset);
+void (*serial_out)(unsigned long addr, int offset, int value);
+
 unsigned long early_serial_base;
 
 #include "../early_serial_console.c"
